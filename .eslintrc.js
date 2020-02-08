6a9c2ffb9b62f47bb4af0c6ee6e3adc08caa895e
rob <robertaxelkirby@gmail.com>
2020-02-08 07:59:45 +0000

    Configure eslint
    Installed eslinlint-plugin-react and eslint-plugin-react-hooks

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+	  node: true,
+    es6: true,
+	  jest: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {}
+};
